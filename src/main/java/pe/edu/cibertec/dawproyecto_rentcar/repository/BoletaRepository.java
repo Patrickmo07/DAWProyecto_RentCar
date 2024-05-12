4e1585dc409f1e82bda14611fbd9d7aa77df00ae
Patrickmo07 <patrick.3.mo@gmail.com>
2024-05-11 21:28:40 -0500

    Login y Mant Usuarios Completados

--- src/main/java/pe/edu/cibertec/dawproyecto_rentcar/repository/BoletaRepository.java
+++ src/main/java/pe/edu/cibertec/dawproyecto_rentcar/repository/BoletaRepository.java
@@ -2,8 +2,8 @@ package com.example.DAWProyecto_RentCar.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.example.DAWProyecto_RentCar.Model.BD.BoletaModel;
+import com.example.DAWProyecto_RentCar.;
 
 @Repository
-public interface BoletaRepository extends JpaRepository<BoletaModel, Integer>{
+public interface BoletaRepository extends JpaRepository<Boleta, Integer>{
 }
